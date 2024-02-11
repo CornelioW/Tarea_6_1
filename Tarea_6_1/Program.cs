dc170fb7aea0281be68718ae0b2d17e2dbdc3773
Cornelio Williams Van Der Werff <cornelio.williamsvander@iti.edu.ec>
2024-02-11 14:43:45 -0500

    Add project files.

---
+++ Tarea_6_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stack procesosListos = new Stack();
+
+        Queue procesosEjecucion = new Queue();
+                
+        procesosListos.Push("Inicializar el juego");
+        procesosListos.Push("Pantalla de carga del juego");
+        procesosListos.Push("Abrir el juego");
+
+        Console.WriteLine("Simulación de planificador de tareas:");
+        while (procesosListos.Count > 0 || procesosEjecucion.Count > 0)
+        {
+            // Si no hay procesos en ejecución, tomar uno de la pila de procesos listos
+            if (procesosEjecucion.Count == 0 && procesosListos.Count > 0)
+            {
+                string proceso = (string)procesosListos.Pop();
+                procesosEjecucion.Enqueue(proceso);
+                Console.WriteLine($"Se está ejecutando: {proceso}");
+            }
+
+            // Ejecutar el primer proceso de la cola de procesos en ejecución
+            if (procesosEjecucion.Count > 0)
+            {
+                string procesoEnEjecucion = (string)procesosEjecucion.Peek();
+                Console.WriteLine($"Proceso en ejecución: {procesoEnEjecucion}");
+
+                procesosEjecucion.Dequeue();
+            }
+
+            // Simular una pausa entre cada paso de la simulación
+            Console.WriteLine("Presiona Enter para continuar...");
+            Console.ReadLine();
+        }
+
+        Console.WriteLine("Todos los procesos han sido ejecutados.");
+    }
+}
